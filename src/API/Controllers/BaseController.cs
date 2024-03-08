769a259eea52cf09564e108a54287cb92209960e
Emopusta <emreduman225@hotmail.com>
2024-03-08 21:40:42 +0300

    Add API controllers and some service registrations with other configurations.

---
+++ src/API/Controllers/BaseController.cs
@@ -0,0 +1,24 @@
+﻿using Core.Utils.Results;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseController : ControllerBase
+    {
+        protected IMediator Mediator =>
+        _mediator ??=
+            HttpContext.RequestServices.GetService<IMediator>()
+            ?? throw new InvalidOperationException("IMediator cannot be retrieved from request services.");
+
+        private IMediator? _mediator;
+
+
+        protected SuccessDataResult<T> Success<T>(T result)
+        {
+            return new SuccessDataResult<T>(result);
+        }
+    }
+}
---
+++ src/API/Controllers/EntitiesController.cs
@@ -0,0 +1,19 @@
+﻿using Application.Features.Entities.Commands.Create;
+using Core.Utils.Results;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EntitiesController : BaseController
+    {
+        [HttpPost]
+        public async Task<IDataResult<CreatedEntityResponse>> Create([FromBody] string name, CancellationToken cancellationToken)
+        {
+            var command = new CreateEntityCommand() { Name = name };
+            var response = await Mediator.Send(command, cancellationToken);
+            return Success(response);
+        }
+    }
+}
