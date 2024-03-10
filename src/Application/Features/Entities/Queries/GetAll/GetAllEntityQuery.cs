8f800f599302b6346c4b2a88d4da5a2e903e20a4
Emopusta <emreduman225@hotmail.com>
2024-03-10 13:31:17 +0300

    Add get all entities query.

---
+++ src/Application/Features/Entities/Queries/GetAll/GetAllEntityQuery.cs
@@ -0,0 +1,8 @@
+﻿using Core.Application.Pipelines;
+using Core.Application.Responses;
+
+namespace Application.Features.Entities.Queries.GetAll;
+
+public class GetAllEntityQuery : IQueryRequest<ListResponse<GetAllEntityListItemDto>>
+{
+}
---
+++ src/Application/Features/Entities/Queries/GetAll/GetAllEntityQueryHandler.cs
@@ -0,0 +1,38 @@
+﻿using Core.Application.GenericRepository;
+using Core.Application.Pipelines;
+using Core.Application.Responses;
+using Domain.Models;
+
+namespace Application.Features.Entities.Queries.GetAll;
+
+public class GetAllEntityQueryHandler : IQueryRequestHandler<GetAllEntityQuery, ListResponse<GetAllEntityListItemDto>>
+{
+    private readonly IGenericRepository<Entity> _entityRepository;
+
+    public GetAllEntityQueryHandler(IGenericRepository<Entity> entityRepository)
+    {
+        _entityRepository = entityRepository;
+    }
+
+    public async Task<ListResponse<GetAllEntityListItemDto>> Handle(GetAllEntityQuery request, CancellationToken cancellationToken)
+    {
+        var entities = await _entityRepository.GetListAsync(cancellationToken: cancellationToken);
+        
+        var response = new ListResponse<GetAllEntityListItemDto>();
+
+        // You can use AutoMapper, Mapster... instead
+        
+        foreach (var entity in entities.Items) 
+        {
+            var mappedListItemDto = new GetAllEntityListItemDto()
+            {
+                Id = entity.Id,
+                Name = entity.Name,
+            };
+
+            response.Items.Add(mappedListItemDto);
+        }
+
+        return response;
+    }
+}
